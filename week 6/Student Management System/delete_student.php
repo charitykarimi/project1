cba8b655945c6303c771ab91a15743d0224cb231
charitykarimi <karimic922@gmail.com>
2026-06-18 11:49:16 +0300

    added projectcode

--- week 6/Student Management System/delete_student.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'db.php';
-
-$id = $_POST['id'];
-
-$sql = "DELETE FROM students WHERE id='$id'";
-
-mysqli_query($conn, $sql);
-?>
--- week 6/employee/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "db.php";
-
-$id = $_GET['id'];
-
-mysqli_query($conn, "DELETE FROM employees WHERE id=$id");
-
-header("Location: index.php");
-?>
